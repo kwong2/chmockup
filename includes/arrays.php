328fc79ffcaf4d140e9ff07fdc361476d5c6808d
Kevin Wong <k.wong4@yahoo.com>
2017-10-02 17:27:20 -0400

    changed index to php and reworked nav with php

---
+++ includes/arrays.php
@@ -0,0 +1,21 @@
+<?php 
+
+  $navItems = array (
+                array (
+                  "href" => "#about",
+                  "title" => "About"
+                ),
+                array (
+                  "href" => "#team",
+                  "title" => "team"
+                ),
+                array (
+                  "href" => "#menu",
+                  "title" => "team"
+                ),
+                array (
+                  "href" => "#  contact",
+                  "title" => "Contact"
+                ),
+  );
+
--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 
+  <?php 
+    include('includes/arrays.php');
+  ?> 
+
   <head>
 
     <meta charset="utf-8">
@@ -8,7 +12,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Creative - Start Bootstrap Theme</title>
+    <title>China Hollywood</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -31,23 +35,30 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">China Hollywood</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
+
+            <?php 
+              foreach($navItems as $item) {
+                echo "<li class=\"nav-item\">
+                        <a class=\"nav-link js-scroll-trigger\" href=\"#about\">$item[title]</a>";
+              };
+            ?> 
+            <!-- <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#about">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+              <a class="nav-link js-scroll-trigger" href="#menu">Menu</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+              <a class="nav-link js-scroll-trigger" href="#team">Team</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a> -->
             </li>
           </ul>
         </div>
